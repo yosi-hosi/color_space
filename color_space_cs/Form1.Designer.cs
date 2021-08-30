6e3a685a9fb2399e0e6a410fb037b0ca2382ec5b
yossy <m034173b@ms-license.oicte.hokudai.ac.jp>
2021-08-30 18:45:35 +0900

    8/30

--- color_space_cs/Form1.Designer.cs
+++ color_space_cs/Form1.Designer.cs
@@ -30,11 +30,47 @@ namespace color_space_cs
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(261, 224);
+            this.button1.Location = new System.Drawing.Point(45, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -42,21 +78,359 @@ namespace color_space_cs
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(471, 10);
+            this.trackBar1.Maximum = 178;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(177, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(384, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "H_MIN";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(384, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 23);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "H_MAX";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(384, 150);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 23);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "S_MIN";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(384, 220);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(48, 23);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "S_MAX";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(384, 290);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(48, 23);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "V_MIN";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(384, 360);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(48, 23);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Text = "V_MAX";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(471, 80);
+            this.trackBar2.Maximum = 179;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar2.Size = new System.Drawing.Size(177, 45);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.TickFrequency = 20;
+            this.trackBar2.Value = 40;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(471, 150);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(177, 45);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.TickFrequency = 20;
+            this.trackBar3.Value = 20;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(471, 220);
+            this.trackBar4.Maximum = 255;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(177, 45);
+            this.trackBar4.TabIndex = 10;
+            this.trackBar4.TickFrequency = 20;
+            this.trackBar4.Value = 170;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(471, 290);
+            this.trackBar5.Maximum = 255;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(177, 45);
+            this.trackBar5.TabIndex = 11;
+            this.trackBar5.TickFrequency = 20;
+            this.trackBar5.Value = 30;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar6.Location = new System.Drawing.Point(471, 360);
+            this.trackBar6.Maximum = 255;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(177, 45);
+            this.trackBar6.TabIndex = 12;
+            this.trackBar6.TickFrequency = 20;
+            this.trackBar6.Value = 200;
+            this.trackBar6.Scroll += new System.EventHandler(this.trackBar6_Scroll);
+            // 
+            // textBox7
+            // 
+            this.textBox7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox7.Location = new System.Drawing.Point(438, 10);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(38, 23);
+            this.textBox7.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(623, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "179";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(480, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 15);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(623, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 15);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "179";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(480, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(623, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 15);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "255";
+            // 
+            // textBox8
+            // 
+            this.textBox8.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox8.Location = new System.Drawing.Point(438, 80);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(38, 23);
+            this.textBox8.TabIndex = 20;
+            // 
+            // textBox9
+            // 
+            this.textBox9.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox9.Location = new System.Drawing.Point(438, 150);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(38, 23);
+            this.textBox9.TabIndex = 21;
+            // 
+            // textBox10
+            // 
+            this.textBox10.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox10.Location = new System.Drawing.Point(438, 220);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(38, 23);
+            this.textBox10.TabIndex = 22;
+            // 
+            // textBox11
+            // 
+            this.textBox11.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox11.Location = new System.Drawing.Point(438, 290);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(38, 23);
+            this.textBox11.TabIndex = 23;
+            // 
+            // textBox12
+            // 
+            this.textBox12.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.textBox12.Location = new System.Drawing.Point(438, 360);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(38, 23);
+            this.textBox12.TabIndex = 24;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(480, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 15);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(623, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 15);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "255";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(480, 320);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 15);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(623, 320);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 15);
+            this.label10.TabIndex = 28;
+            this.label10.Text = "255";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(480, 390);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 15);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(623, 390);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(25, 15);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "255";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "0";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- color_space_cs/Form1.cs
+++ color_space_cs/Form1.cs
@@ -11,15 +11,15 @@ using System.Windows.Forms;
 
 namespace color_space_cs
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
 		private void button1_Click(object sender, EventArgs e)
-        {
+		{
 			string filepath = "//ares//Temporary//EV//tsurunuma//2020//20201005tsurunuma//Sony camera//MAH00023.avi";
 			//string filepath = "MAH00023.avi";
 			VideoCapture video = new VideoCapture();
@@ -31,20 +31,16 @@ namespace color_space_cs
 			}
 
 			int H_MIN = 0, H_MAX = 40, S_MIN = 20, S_MAX = 170, V_MIN = 30, V_MAX = 200;
-			string HSV_Trackbar = "Trackbars HSV";
-			Cv2.NamedWindow(HSV_Trackbar, 0);
-			Cv2.ResizeWindow(HSV_Trackbar, 400, 150);
-			Cv2.CreateTrackbar("H_MIN", HSV_Trackbar, 179, onChange: H_MIN_Changed);
-			Cv2.CreateTrackbar("H_MAX", HSV_Trackbar, &H_MAX, 179, on_trackbar);
-			Cv2.CreateTrackbar("S_MIN", HSV_Trackbar, &S_MIN, 255, on_trackbar);
-			Cv2.CreateTrackbar("S_MAX", HSV_Trackbar, &S_MAX, 255, on_trackbar);
-			Cv2.CreateTrackbar("V_MIN", HSV_Trackbar, &V_MIN, 255, on_trackbar);
-			Cv2.CreateTrackbar("V_MAX", HSV_Trackbar, &V_MAX, 255, on_trackbar);
+			textBox7.Text = H_MIN.ToString();
+			textBox8.Text = H_MAX.ToString();
+			textBox9.Text = S_MIN.ToString();
+			textBox10.Text = S_MAX.ToString();
+			textBox11.Text = V_MIN.ToString();
+			textBox12.Text = V_MAX.ToString();
 
 			Mat image = new Mat();
 			Mat HSV = new Mat();
 			Mat binary = new Mat();
-			Cv2.NamedWindow("image");
 			while (true)
 			{
 				video.Read(image);
@@ -56,12 +52,50 @@ namespace color_space_cs
 				Cv2.ImShow("HSV", HSV);
 				Cv2.ImShow("binary", binary);
 				if (Cv2.WaitKey(1) == 'q') break;
+				H_MIN = trackBar1.Value;
+				H_MAX = trackBar2.Value;
+				S_MIN = trackBar3.Value;
+				S_MAX = trackBar4.Value;
+				V_MIN = trackBar5.Value;
+				V_MAX = trackBar6.Value;
+
 			}
 			Cv2.DestroyAllWindows();
 			return;
 		}
-	}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			textBox7.Text = trackBar1.Value.ToString();
+		}
+
+		private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+			textBox8.Text = trackBar2.Value.ToString();
+		}
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+			textBox9.Text = trackBar3.Value.ToString();
+		}
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+			textBox10.Text = trackBar4.Value.ToString();
+		}
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+			textBox11.Text = trackBar5.Value.ToString();
+		}
+
+        private void trackBar6_Scroll(object sender, EventArgs e)
+        {
+			textBox12.Text = trackBar6.Value.ToString();
+		}
+    }
 }
+
 
 //基本は「考えられる修正内容」のそれっぽいやつでいける
 //Cv2.Imshowみたいな感じに修正
